064fcca8f140e8766b95c0065599bebc075e4154
K Tharun Sai <tharunsaik@users.noreply.github.com>
2020-11-25 19:35:07 +0530

    index

---
+++ index.php
@@ -0,0 +1,269 @@
+<!--
+Header Start
+-->
+<?php include 'header.php';?>
+<!--
+Header End
+-->
+<!--
+Main Content Start
+-->
+<!--
+Slider Start
+-->
+<div class="uk-position-relative uk-visible-toggle uk-light home-slider" tabindex="-1" uk-slideshow="autoplay: true">
+
+    <ul class="uk-slideshow-items" uk-height-viewport>
+        <li>
+            <img src="https://picsum.photos/id/1043/1920/1080" alt="" uk-cover>
+        </li>
+        <li>
+            <img src="https://picsum.photos/id/1055/1920/1080" alt="" uk-cover>
+        </li>
+        <li>
+            <img src="https://picsum.photos/id/1063/1920/1080" alt="" uk-cover>
+        </li>
+    </ul>
+
+    <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+
+</div>
+<!--
+Slider End
+-->
+<!--
+Destinations Start
+-->
+<section class="py-5">
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<h2 class="uk-heading-line uk-text-center uk-text-primary uk-text-uppercase"><span class="border-orange-500-before border-orange-500-after">Destation Deals</span></h2>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="uk-child-width-1-1 uk-child-width-1-4@s uk-grid-match uk-grid-small uk-margin" uk-grid>
+			<a class="uk-link-toggle" href="#">
+				<div class="uk-inline-clip uk-transition-toggle" tabindex="0">
+					<img src="https://picsum.photos/id/973/1920/1080" alt="">
+					<div class="uk-transition-slide-left uk-position-top uk-overlay uk-overlay-primary uk-padding-small uk-margin-large-right uk-margin-top">
+						<h3 class="font-size-9 uk-margin-remove">Golconda Fort</h3>
+						<span class="uk-text-warning">View Deal</span>
+					</div>
+				</div>
+			</a>
+			<a class="uk-link-toggle" href="#">
+				<div class="uk-inline-clip uk-transition-toggle" tabindex="0">
+					<img src="https://picsum.photos/id/988/1920/1080" alt="">
+					<div class="uk-transition-slide-left uk-position-top uk-overlay uk-overlay-primary uk-padding-small uk-margin-large-right uk-margin-top">
+						<h3 class="font-size-9 uk-margin-remove">Golconda Fort</h3>
+						<span class="uk-text-warning">View Deal</span>
+					</div>
+				</div>
+			</a>
+			<a class="uk-link-toggle" href="#">
+				<div class="uk-inline-clip uk-transition-toggle" tabindex="0">
+					<img src="https://picsum.photos/id/989/1920/1080" alt="">
+					<div class="uk-transition-slide-left uk-position-top uk-overlay uk-overlay-primary uk-padding-small uk-margin-large-right uk-margin-top">
+						<h3 class="font-size-9 uk-margin-remove">Golconda Fort</h3>
+						<span class="uk-text-warning">View Deal</span>
+					</div>
+				</div>
+			</a>
+			<a class="uk-link-toggle" href="#">
+				<div class="uk-inline-clip uk-transition-toggle" tabindex="0">
+					<img src="https://picsum.photos/id/993/1920/1080" alt="">
+					<div class="uk-transition-slide-left uk-position-top uk-overlay uk-overlay-primary uk-padding-small uk-margin-large-right uk-margin-top">
+						<h3 class="font-size-9 uk-margin-remove">Golconda Fort</h3>
+						<span class="uk-text-warning">View Deal</span>
+					</div>
+				</div>
+			</a>
+			<a class="uk-link-toggle" href="#">
+				<div class="uk-inline-clip uk-transition-toggle" tabindex="0">
+					<img src="https://picsum.photos/id/944/1920/1080" alt="">
+					<div class="uk-transition-slide-left uk-position-top uk-overlay uk-overlay-primary uk-padding-small uk-margin-large-right uk-margin-top">
+						<h3 class="font-size-9 uk-margin-remove">Ramoji Film City</h3>
+						<span class="uk-text-warning">View Deal</span>
+					</div>
+				</div>
+			</a>
+			<a class="uk-link-toggle" href="#">
+				<div class="uk-inline-clip uk-transition-toggle" tabindex="0">
+					<img src="https://picsum.photos/id/964/1920/1080" alt="">
+					<div class="uk-transition-slide-left uk-position-top uk-overlay uk-overlay-primary uk-padding-small uk-margin-large-right uk-margin-top">
+						<h3 class="font-size-9 uk-margin-remove">Golconda Fort</h3>
+						<span class="uk-text-warning">View Deal</span>
+					</div>
+				</div>
+			</a>
+			<a class="uk-link-toggle" href="#">
+				<div class="uk-inline-clip uk-transition-toggle" tabindex="0">
+					<img src="https://picsum.photos/id/933/1920/1080" alt="">
+					<div class="uk-transition-slide-left uk-position-top uk-overlay uk-overlay-primary uk-padding-small uk-margin-large-right uk-margin-top">
+						<h3 class="font-size-9 uk-margin-remove">Golconda Fort</h3>
+						<span class="uk-text-warning">View Deal</span>
+					</div>
+				</div>
+			</a>
+			<a class="uk-link-toggle" href="#">
+				<div class="uk-background-primary uk-light uk-padding uk-panel font-size-9 text-white uk-flex-center align-content-center">
+				Destinations Guide
+					<span uk-icon="icon: arrow-right"></span>
+				</div>
+			</a>
+		</div>
+	</div>
+</section>
+<!--
+Destinations End
+-->
+<!--
+Destination Fleets Start
+-->
+<section class="section-before bg-orange-50 pt-10 pb-5">
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<h2 class="uk-heading-line uk-text-center uk-text-primary uk-text-uppercase"><span class="border-orange-500-before border-orange-500-after mt-5">TOP DESTINATIONS AROUND THE Country</span></h2>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row mb-4">
+			<div class="col">
+				<div class="pb_entry">
+					<div class="d-table table-view">
+						<div class="col-sm cell-view pb_header bg-white uk-cover-container">
+							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Charminar_sumeet_photography_3.JPG/280px-Charminar_sumeet_photography_3.JPG" alt="" uk-cover>
+						</div>
+						<div class="col-sm cell-view pb_content bg-grey-6">
+							<div class="pb-title color-dark-2">Golconda Fort</div>
+							<div class="pb-price color-dark-2">from <span class="color-red-3">₹860</span></div>
+							<div class="pb-text color-grey-3"><img class="plane-icon" src="img/plane.png" alt=""> 35 Airlines</div>
+							<div class="pb-text color-grey-3"><img class="location-icon" src="img/loc_icon_small_grey.png" alt="">90 destination</div>
+						</div>
+						<div class="col-sm cell-view pb_content bg-grey-6">
+							<div class="pb-title color-dark-2">Charminar</div>
+							<div class="pb-price color-dark-2">from <span class="color-red-3">₹860</span></div>
+							<div class="pb-text color-grey-3"><img class="plane-icon" src="img/plane.png" alt=""> 35 Airlines</div>
+							<div class="pb-text color-grey-3"><img class="location-icon" src="img/loc_icon_small_grey.png" alt="">90 destination</div>
+						</div>
+						<div class="col-sm cell-view pb_content bg-grey-6">
+							<div class="pb-title color-dark-2">Slar Jung Museum</div>
+							<div class="pb-price color-dark-2">from <span class="color-red-3">₹860</span></div>
+							<div class="pb-text color-grey-3"><img class="plane-icon" src="img/plane.png" alt=""> 35 Airlines</div>
+							<div class="pb-text color-grey-3"><img class="location-icon" src="img/loc_icon_small_grey.png" alt="">90 destination</div>
+						</div>
+						<div class="col-sm cell-view pb_content bg-grey-6">
+							<div class="pb-title color-dark-2">Hussian Sagar</div>
+							<div class="pb-price color-dark-2">from <span class="color-red-3">₹860</span></div>
+							<div class="pb-text color-grey-3"><img class="plane-icon" src="img/plane.png" alt=""> 35 Airlines</div>
+							<div class="pb-text color-grey-3"><img class="location-icon" src="img/loc_icon_small_grey.png" alt="">90 destination</div>
+						</div>
+						<div class="col-sm cell-view pb_check bg-white">
+							<div class="pb_check_title color-dark-2">Hyderabad Trip</div>
+							<div class="pb_check_text color-grey-3">Nunc cursus libero purus ac congue arcu cursus ut .</div>
+							<a href="#" class="uk-button uk-button-danger">Book Fleet</a>
+						</div>															
+					</div>			
+				</div>
+			</div>
+		</div>
+		<div class="row mb-4">
+			<div class="col">
+				<div class="pb_entry">
+					<div class="d-table table-view">
+						<div class="col-sm cell-view pb_header bg-white uk-cover-container">
+							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/DGTF2318.jpg/1024px-DGTF2318.jpg" alt="" uk-cover>
+						</div>
+						<div class="col-sm cell-view pb_content bg-grey-6">
+							<div class="pb-title color-dark-2">Golconda Fort</div>
+							<div class="pb-price color-dark-2">from <span class="color-red-3">₹860</span></div>
+							<div class="pb-text color-grey-3"><img class="plane-icon" src="img/plane.png" alt=""> 35 Airlines</div>
+							<div class="pb-text color-grey-3"><img class="location-icon" src="img/loc_icon_small_grey.png" alt="">90 destination</div>
+						</div>
+						<div class="col-sm cell-view pb_content bg-grey-6">
+							<div class="pb-title color-dark-2">Charminar</div>
+							<div class="pb-price color-dark-2">from <span class="color-red-3">₹860</span></div>
+							<div class="pb-text color-grey-3"><img class="plane-icon" src="img/plane.png" alt=""> 35 Airlines</div>
+							<div class="pb-text color-grey-3"><img class="location-icon" src="img/loc_icon_small_grey.png" alt="">90 destination</div>
+						</div>
+						<div class="col-sm cell-view pb_content bg-grey-6">
+							<div class="pb-title color-dark-2">Slar Jung Museum</div>
+							<div class="pb-price color-dark-2">from <span class="color-red-3">₹860</span></div>
+							<div class="pb-text color-grey-3"><img class="plane-icon" src="img/plane.png" alt=""> 35 Airlines</div>
+							<div class="pb-text color-grey-3"><img class="location-icon" src="img/loc_icon_small_grey.png" alt="">90 destination</div>
+						</div>
+						<div class="col-sm cell-view pb_content bg-grey-6">
+							<div class="pb-title color-dark-2">Hussian Sagar</div>
+							<div class="pb-price color-dark-2">from <span class="color-red-3">₹860</span></div>
+							<div class="pb-text color-grey-3"><img class="plane-icon" src="img/plane.png" alt=""> 35 Airlines</div>
+							<div class="pb-text color-grey-3"><img class="location-icon" src="img/loc_icon_small_grey.png" alt="">90 destination</div>
+						</div>
+						<div class="col-sm cell-view pb_check bg-white">
+							<div class="pb_check_title color-dark-2">Karnataka Trip</div>
+							<div class="pb_check_text color-grey-3">Nunc cursus libero purus ac congue arcu cursus ut .</div>
+							<a href="#" class="uk-button uk-button-danger">Book Fleet</a>
+						</div>															
+					</div>			
+				</div>
+			</div>
+		</div>
+		<div class="row mb-4">
+			<div class="col">
+				<div class="pb_entry">
+					<div class="d-table table-view">
+						<div class="col-sm cell-view pb_header bg-white uk-cover-container">
+							<img src="https://upload.wikimedia.org/wikipedia/commons/e/ed/Palolem_beach.jpg" alt="" uk-cover>
+						</div>
+						<div class="col-sm cell-view pb_content bg-grey-6">
+							<div class="pb-title color-dark-2">Golconda Fort</div>
+							<div class="pb-price color-dark-2">from <span class="color-red-3">₹860</span></div>
+							<div class="pb-text color-grey-3"><img class="plane-icon" src="img/plane.png" alt=""> 35 Airlines</div>
+							<div class="pb-text color-grey-3"><img class="location-icon" src="img/loc_icon_small_grey.png" alt="">90 destination</div>
+						</div>
+						<div class="col-sm cell-view pb_content bg-grey-6">
+							<div class="pb-title color-dark-2">Charminar</div>
+							<div class="pb-price color-dark-2">from <span class="color-red-3">₹860</span></div>
+							<div class="pb-text color-grey-3"><img class="plane-icon" src="img/plane.png" alt=""> 35 Airlines</div>
+							<div class="pb-text color-grey-3"><img class="location-icon" src="img/loc_icon_small_grey.png" alt="">90 destination</div>
+						</div>
+						<div class="col-sm cell-view pb_content bg-grey-6">
+							<div class="pb-title color-dark-2">Slar Jung Museum</div>
+							<div class="pb-price color-dark-2">from <span class="color-red-3">₹860</span></div>
+							<div class="pb-text color-grey-3"><img class="plane-icon" src="img/plane.png" alt=""> 35 Airlines</div>
+							<div class="pb-text color-grey-3"><img class="location-icon" src="img/loc_icon_small_grey.png" alt="">90 destination</div>
+						</div>
+						<div class="col-sm cell-view pb_content bg-grey-6">
+							<div class="pb-title color-dark-2">Hussian Sagar</div>
+							<div class="pb-price color-dark-2">from <span class="color-red-3">₹860</span></div>
+							<div class="pb-text color-grey-3"><img class="plane-icon" src="img/plane.png" alt=""> 35 Airlines</div>
+							<div class="pb-text color-grey-3"><img class="location-icon" src="img/loc_icon_small_grey.png" alt="">90 destination</div>
+						</div>
+						<div class="col-sm cell-view pb_check bg-white">
+							<div class="pb_check_title color-dark-2">Goa</div>
+							<div class="pb_check_text color-grey-3">Nunc cursus libero purus ac congue arcu cursus ut .</div>
+							<a href="#" class="uk-button uk-button-danger">Book Fleet</a>
+						</div>															
+					</div>			
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--
+Destination Fleets End
+-->
+<!--
+Main Content End
+-->
+<!--
+Footer Start
+-->
+<?php include 'footer.php';?>
+<!--
+Footer End
+-->
